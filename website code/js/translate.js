308ffc63c9bad3eff8f67a057bd64f05a4f1d79b
JClolo <gilolojhua@gmail.com>
2022-07-01 17:59:19 +1000

    create translate.js

---
+++ website code/js/translate.js
@@ -0,0 +1,10 @@
+
+		function setLanguage(lang) {
+			$('#furniture_tag').text(lang.furniture_tag);
+			$('#tutorial_tag').text(lang.tutorial_tag);
+		}
+
+		function clearLanguage() {
+			$('#en').css('background-color', 'transparent');
+			$('#cn').css('background-color', 'transparent');
+		}
