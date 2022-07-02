48ee6e6a49494a3cad045a529e43e5c90b1b37fb
Junhao_Li <jlii0168@student.monash.edu>
2022-07-02 13:59:42 +1000

    index translation done

--- website code/js/translate.js
+++ website code/js/translate.js
@@ -3,12 +3,12 @@
 			$('#furniture_tag').text(lang.furniture_tag);
 			$('#tutorial_tag').text(lang.tutorial_tag);
 			$('#furnture_assembly_tag').text(lang.furniture_tag);
-			$('#pra_tag').text(lang.furniture_tag);
+			$('#pra_tag').text(lang.pra_tag);
 			$('#read_more').text(lang.read_more);
 			$('#read_more_tut').text(lang.read_more);
 			$('#pra_tag_tutor').text(lang.pra_tag_tutor);
+			$('#scan_code').text(lang.scan_code);
 			
-
 		}
 
 		function clearLanguage() {
