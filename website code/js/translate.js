c06c0f858743b3c90dd24991ce5844e761f10ec6
Junhao_Li <jlii0168@student.monash.edu>
2022-07-02 13:43:46 +1000

    translation update

--- website code/js/translate.js
+++ website code/js/translate.js
@@ -2,6 +2,13 @@
 		function setLanguage(lang) {
 			$('#furniture_tag').text(lang.furniture_tag);
 			$('#tutorial_tag').text(lang.tutorial_tag);
+			$('#furnture_assembly_tag').text(lang.furniture_tag);
+			$('#pra_tag').text(lang.furniture_tag);
+			$('#read_more').text(lang.read_more);
+			$('#pra_tag_tutor').text(lang.read_more);
+			$('#pra_tag_tutor').text(lang.pra_tag_tutor);
+			
+
 		}
 
 		function clearLanguage() {
