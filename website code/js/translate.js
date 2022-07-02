d9c3187cc39456c563e8baf840b726dc3826af39
JClolo <gilolojhua@gmail.com>
2022-07-02 15:12:02 +1000

    update tutor translate

--- website code/js/translate.js
+++ website code/js/translate.js
@@ -13,13 +13,17 @@
 			$('#furniture_content').text(lang.pra_tag);
 			$('#Furnture_range').text(lang.Furnture_range);
 			$('#furniture_intro').text(lang.furniture_intro);
+			$('#tutor1_title').text(lang.tutor1_title);
+			$('#tutor2_title').text(lang.tutor2_title);
+			$('#tutor1_intro').text(lang.tutor1_intro);
+			$('#tutor2_intro').text(lang.tutor2_intro);
+			$('#tutor1_contact').text(lang.tutor1_contact);
+			$('#tutor2_contact').text(lang.tutor2_contact);
+			$('#scan_tut_code').text(lang.scan_code);
+			$('#close_btn').text(lang.close_detail);
 		}
 
 		function clearLanguage() {
 			$('#en').css('background-color', 'transparent');
 			$('#cn').css('background-color', 'transparent');
-		}
-
-		function setTutor1Language() {
-			
 		}
